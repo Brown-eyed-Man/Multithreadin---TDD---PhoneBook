59795fa3eb1ace5f5ce8fee9e72cd94134b8c0b6
Alexey Klyuchnikov <klyuchnikov.alexey@yandex.ru>
2024-05-05 17:56:39 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,11 @@
 package ru.netology;
 
+import java.util.*;
+
 public class PhoneBook {
+    private static Map<String, String> phoneBook = new HashMap<>();
     public static int add(String name, String number) {
-        return 0;
+        phoneBook.putIfAbsent(name, number);
+        return phoneBook.size();
     }
 }
